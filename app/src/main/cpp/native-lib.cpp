4b3fa9303bbdb029faab87f2ba91f3645565a157
Umidjan <97349110+umidjandev@users.noreply.github.com>
2022-07-23 22:29:47 +0500

    ndk calculator

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,35 @@
 #include <jni.h>
 #include <string>
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_najdimu_ndkcalculator_MainActivity_stringFromJNI(
+extern "C" JNIEXPORT jdouble JNICALL
+Java_com_najdimu_ndkcalculator_MainActivity_add(
         JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Error something...";
-    return env->NewStringUTF(hello.c_str());
+        jobject /* this */,
+        jdouble a, jdouble b) {
+    return a+b;
 }
+
+extern "C" JNIEXPORT jdouble JNICALL
+Java_com_najdimu_ndkcalculator_MainActivity_sub(
+        JNIEnv* env,
+        jobject /* this */,
+        jdouble a, jdouble b) {
+    return a-b;
+}
+
+extern "C" JNIEXPORT jdouble JNICALL
+Java_com_najdimu_ndkcalculator_MainActivity_mut(
+        JNIEnv* env,
+        jobject /* this */,
+        jdouble a, jdouble b) {
+    return a*b;
+}
+
+extern "C" JNIEXPORT jdouble JNICALL
+Java_com_najdimu_ndkcalculator_MainActivity_div(
+        JNIEnv* env,
+        jobject /* this */,
+        jdouble a, jdouble b) {
+    return a/b;
+}
+
--- app/src/main/java/com/najdimu/ndkcalculator/MainActivity.kt
+++ app/src/main/java/com/najdimu/ndkcalculator/MainActivity.kt
@@ -2,170 +2,153 @@ package com.najdimu.ndkcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.method.ScrollingMovementMethod
-import android.util.Log
-import androidx.appcompat.app.AppCompatDelegate
-import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
-import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
-import com.najdimu.ndkcalculator.databinding.ActivityMainBinding
-import javax.script.ScriptEngine
-import javax.script.ScriptEngineManager
-import javax.script.ScriptException
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-    private var check = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
+    }
 
-        binding.darkMode.setOnCheckedChangeListener { buttonView, isChecked ->
-            if (isChecked) {
-                AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
-            }else{
-                AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
+    fun buNumberEvent(view: View){
+        if(isNewOp){
+            etShowNumber.setText("")
+        }
+        isNewOp=false
+
+        val buSelect= view as Button
+        var buClickValue:String=etShowNumber.text.toString()
+        when(buSelect.id){
+            bu00.id->{
+                buClickValue+="00"
+            }
+            bu0.id->{
+                buClickValue+="0"
+            }
+            bu1.id->{
+                buClickValue+="1"
+            }
+            bu2.id->{
+                buClickValue+="2"
+            }
+            bu3.id->{
+                buClickValue+="3"
+            }
+            bu4.id->{
+                buClickValue+="4"
+            }
+            bu5.id->{
+                buClickValue+="5"
+            }
+            bu6.id->{
+                buClickValue+="6"
+            }
+            bu7.id->{
+                buClickValue+="7"
+            }
+            bu8.id->{
+                buClickValue+="8"
+            }
+            bu9.id->{
+                buClickValue+="9"
+            }
+            buDot.id->{
+                //TODO: prevent adding morve than 1 dot
+                buClickValue+="."
+            }
+            buPlusMins.id->{
+                buClickValue= "-$buClickValue"
             }
         }
+        etShowNumber.setText(buClickValue)
+    }
 
-        binding.button1.setOnClickListener{
-            button("1")
+
+    var op="*"
+    var oldNumber=""
+    var isNewOp=true
+    fun buOpEevent(view: View){
+
+        val buSelect= view as Button
+        when(buSelect.id) {
+            buMul.id -> {
+
+                op="*"
+
+
+            }
+            buDiv.id -> {
+
+                op="/"
+            }
+            buSub.id -> {
+
+
+                op="-"
+            }
+            buSum.id -> {
+
+                op="+"
+            }
+
         }
+        oldNumber=etShowNumber.text.toString()
+        isNewOp=true
+    }
 
-        binding.button2.setOnClickListener{
-            button("2")
-        }
+    fun buEqualEvent(view: View){
+        val newNumber=etShowNumber.text.toString()
+        var finalNumber:Double?=null
+        when(op){
 
-        binding.button3.setOnClickListener{
-           button("3")
-        }
-
-        binding.button4.setOnClickListener{
-            button("4")
-        }
-
-        binding.button5.setOnClickListener{
-            button("5")
-        }
-
-        binding.button6.setOnClickListener{
-            button("6")
-        }
-
-        binding.button7.setOnClickListener{
-            button("7")
-        }
-
-        binding.button8.setOnClickListener{
-            button("8")
-        }
-
-        binding.button9.setOnClickListener{
-            button("9")
-        }
-
-        binding.button0.setOnClickListener{
-            button("0")
-        }
-
-        binding.button00.setOnClickListener{
-            button("00")
-        }
-
-        binding.buttonDot.setOnClickListener{
-            button(".")
-        }
-
-        binding.buttonAdd.setOnClickListener{
-            operator("+")
-        }
-
-        binding.buttonSub.setOnClickListener{
-            operator("-")
-        }
-
-        binding.buttonMul.setOnClickListener{
-            operator("*")
-        }
-
-        binding.buttonDiv.setOnClickListener{
-            operator("/")
-        }
-
-        binding.buttonPercent.setOnClickListener{
-            operator("%")
-        }
-
-        binding.buttonEqual.setOnClickListener{
-            binding.formula.text = null
-        }
-
-        binding.buttonClear.setOnClickListener{
-            binding.formula.text = null
-            binding.result.text = null
-        }
-
-        binding.buttonBackspace.setOnClickListener{
-            val BackSpace: String?
-            if(binding.formula.text.length > 0) {
-                val stringBuilder: StringBuilder = StringBuilder(binding.formula.text)
-                val find = binding.formula.text.toString()
-                val find2 = find.last()
-
-                if(find2 == '+' || find2 == '-' || find2 == '*' || find2 == '/' || find2 == '%')
-                {
-                    check -= 1
-                }
-
-                stringBuilder.deleteCharAt(binding.formula.text.length-1)
-                BackSpace = stringBuilder.toString()
-                binding.formula.text = BackSpace
-                result(BackSpace)
+            "*"->{
+                finalNumber= mut(oldNumber.toDouble(),newNumber.toDouble())
+            }
+            "/"->{
+                finalNumber=  div(oldNumber.toDouble(),newNumber.toDouble())
+            }
+            "+"->{
+                finalNumber=  add(oldNumber.toDouble(),newNumber.toDouble())
+            }
+            "-"->{
+                finalNumber= sub(oldNumber.toDouble(),newNumber.toDouble())
             }
         }
+        etShowNumber.setText(finalNumber.toString())
+        isNewOp=true
     }
 
-    private fun result(text: String) {
-        val engine: ScriptEngine = ScriptEngineManager().getEngineByName("rhino")
-        try {
-            if (text.isNotEmpty()) {
-                val result: Any = engine.eval(text)
-                val mainr = result.toString()
+    fun buPercent(view: View){
+        val number:Double=etShowNumber.text.toString().toDouble()/100
 
-                if(check == 0) {
-                    binding.result.text = null
-                }
-                else {
-                    binding.result.text = mainr
-                }
-            }else{
-                binding.result.text = "0"
-            }
-        }
-        catch (e: ScriptException) {
-            Log.d("TAG", stringFromJNI())
-        }
+        etShowNumber.setText(number.toString())
+        isNewOp=true
+
     }
 
-    fun button(button: String) {
-       val text = binding.formula.text.toString()+ button
-        binding.formula.text = text
-        result(text)
+    fun buClean(view: View){
+        etShowNumber.setText("0")
+        isNewOp=true
     }
 
-    fun operator(operator: String) {
-        val text = binding.formula.text.toString()+ operator
-        binding.formula.text = text
-        check += 1
-    }
 
-    external fun stringFromJNI(): String
+
+
+    external fun add(a: Double, b: Double): Double
+    external fun sub(a: Double, b: Double): Double
+    external fun mut(a: Double, b: Double): Double
+    external fun div(a: Double, b: Double): Double
 
     companion object {
+        // Used to load the 'calculator' library on application startup.
         init {
             System.loadLibrary("ndkcalculator")
         }
     }
+
 }
